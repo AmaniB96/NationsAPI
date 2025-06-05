50c532cbf3423611d4f641e6d74a56b0c211bf98
Ali <alijaberoha@hotmail.com>
2025-06-05 11:44:06 +0200

    add quizz

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,27 @@
-import './nav.css'
+import { Link, useLocation } from 'react-router-dom';
+import './nav.css';
 
 export default function Nav() {
-    
-    return(
-        <div className="nav">
-            <div className="logo">
-                <h3>Where in the world ?</h3>
-            </div>
-            <div className='nav-btn'>
-                <p>Quizz</p>
-            </div>
-            <div className="dark-btn">
-                <button className="darkmode">Dark</button>
-            </div>
-        </div>
-    )
+  const location = useLocation();
+
+  return (
+    <div className="nav">
+      <div className="logo">
+        <h3>Where in the world ?</h3>
+      </div>
+
+      <ul className="nav-links">
+        <li className={location.pathname === '/' ? 'active' : ''}>
+          <Link to="/">Home</Link>
+        </li>
+        <li className={location.pathname === '/quizz' ? 'active' : ''}>
+          <Link to="/quizz">Quizz</Link>
+        </li>
+      </ul>
+
+      <div className="dark-btn">
+        <button className="darkmode">Dark</button>
+      </div>
+    </div>
+  );
 }
