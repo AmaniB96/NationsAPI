9c945e4f2f111c965beaeac087b373064b72e881
amani <bataganiramani@gmail.com>
2025-06-05 11:19:44 +0200

    card done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,8 +15,16 @@ export default function Home() {
     return(
         <>
          {data && data.map((item, index) => (            
-            <div key={index}>
-                <img src={item.flags.png}></img>
+            <div className='card-container' key={index}>
+                <div className='flag'>
+                    <img src={item.flags.png}></img>
+                </div>
+                <div className='infos'>
+                    <h3>{item.name.common}</h3>
+                    <p> <b>Population</b>: {item.population} </p>
+                    <p> <b>Region</b>: {item.region}</p>
+                    <p> <b>Capital</b>: {item.capital}</p>
+                </div>
             </div>
          ))}
         </>
